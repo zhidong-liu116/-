4970c1dd8a4521b903170d998b80949d6c4b162c
Eric-Zdl <64428503+zhidong-liu116@users.noreply.github.com>
2023-08-11 05:35:39 -0400

    Update leetcode27 移除元素.py

--- leetcode27 移除元素.py
+++ leetcode27 移除元素.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(len(nums)): #这里的i就是快指针
             if nums[i] != val: # 当快指针指向的元素不等于val的时候，把这个值赋给slow pointer
                 nums[slow] = nums[i] # 赋值操作
-                slow += 1
+                slow += 1 # 记住，这里的if判断过了之后再加1，因为这样才证明这里的值不是val
         return slow
